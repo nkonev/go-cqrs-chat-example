04fa38f94e3bc9d0017c4d6d86e28f7d3d1c6ae9
Nikita Konev <nikit.cpp@yandex.ru>
2025-06-07 02:05:38 +0300

    todo

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "go-cqrs-chat-example/cmd"
 
 // TODO paginate in responses with keyset for chat
+// TODO add timestamp for chat participant and order last n participants by it
 func main() {
 	cmd.Execute()
 }
