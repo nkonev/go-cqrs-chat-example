53f013a30067dfbe79264b904345b23b559276af
Nikita Konev <nikit.cpp@yandex.ru>
2025-06-08 01:58:42 +0300

    clarify comment

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "go-cqrs-chat-example/cmd"
 
-// TODO paginate in responses with keyset for chat
+// TODO paginate in responses messages, participants, blog posts, blog comments
 func main() {
 	cmd.Execute()
 }
